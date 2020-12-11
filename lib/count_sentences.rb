0588e6a9f2388e49c70160140aeca254ae3b4555
Alex Cline <rev.a.r.cline@gmail.com>
2020-12-11 16:07:51 -0600

    commit tree b526e9bf92477f8b378d7579518ccc9f773f21b9

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,21 @@
+# monkey patch some stuff into String
 require 'pry'
 
+# questions and sentences oh my
 class String
-
   def sentence?
-    
+    end_with?('.')
   end
 
   def question?
-
+    end_with?('?')
   end
 
   def exclamation?
-
+    end_with?('!')
   end
 
   def count_sentences
-
+    split(Regexp.union(['. ', '! ', '? '])).count
   end
 end
